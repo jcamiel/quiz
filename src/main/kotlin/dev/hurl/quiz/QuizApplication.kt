3031a9a6f46913c116287dce20126bf1aa6ea89f
jcamiel <jeanchristophe.amiel@gmail.com>
2021-01-29 15:21:53 +0100

    First skeleton for demo project.

---
+++ src/main/kotlin/dev/hurl/quiz/QuizApplication.kt
@@ -0,0 +1,11 @@
+package dev.hurl.quiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuizApplication>(*args)
+}
---
+++ src/test/kotlin/dev/hurl/quiz/QuizApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.hurl.quiz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
