c11a489ae54851d9b945822d02a7f2f33cd63f95
jcamiel <jeanchristophe.amiel@gmail.com>
2021-02-06 16:54:41 +0100

    Update kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.springframework.boot") version "2.4.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.github.ben-manes.versions") version "0.36.0"
-    kotlin("jvm") version "1.4.21-2"
-    kotlin("plugin.spring") version "1.4.21-2"
+    kotlin("jvm") version "1.4.30"
+    kotlin("plugin.spring") version "1.4.30"
 }
 
 val springBootVersion = "2.4.2"
