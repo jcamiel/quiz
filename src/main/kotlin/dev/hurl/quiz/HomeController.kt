e9ead45b59e89af7cdd4a8daa40b060287657974
jcamiel <jeanchristophe.amiel@gmail.com>
2021-02-06 16:53:14 +0100

    Update home controller.

--- src/main/kotlin/dev/hurl/quiz/HomeController.kt
+++ src/main/kotlin/dev/hurl/quiz/HomeController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView
 @Controller
 class HomeController {
 
-
+    // https://lovattspuzzles.com/online-puzzles-competitions/ultimate-online-trivia-quiz/
     @GetMapping(path = ["/"])
     fun get(): ModelAndView {
         val modelAndView = ModelAndView("home")
