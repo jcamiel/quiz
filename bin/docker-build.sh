d9c3214f033ca5c35570398d9efb112e98d4794d
jcamiel <jeanchristophe.amiel@gmail.com>
2021-05-09 18:15:23 +0200

    Add build and publish Github action.

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build -f docker/Dockerfile -t quiz .
+docker build -f docker/Dockerfile -t jcamiel/quiz:latest .
+docker tag jcamiel/quiz:latest ghcr.io/jcamiel/quiz:latest
 # docker run -dp 8080:8080 quiz
---
+++ bin/docker-publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker push ghcr.io/jcamiel/quiz:latest
